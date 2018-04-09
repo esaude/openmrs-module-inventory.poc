6eaf1612ef69ebc72ef87d31ff6fb6819c27e3a0
Guimino <minoneves@gmail.com>
2018-04-09 10:01:46 +0200

    more adjusts

--- api/src/main/java/org/openmrs/module/inventorypoc/batch/service/BatchServiceImpl.java
+++ api/src/main/java/org/openmrs/module/inventorypoc/batch/service/BatchServiceImpl.java
@@ -36,59 +36,60 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class BatchServiceImpl extends BaseOpenmrsService implements BatchService {
-	
+
 	private BatchDAO batchDAO;
-	
+
 	private BatchEntryDAO batchEntryDAO;
-	
+
 	@Autowired
 	private BatchValidator batchValidator;
-	
+
 	private static final Double ZERO_DOUBLE_VALUE = Double.valueOf(0);
-	
+
 	@Override
 	public void setBatchDAO(final BatchDAO batchDAO) {
 		this.batchDAO = batchDAO;
 	}
-	
+
 	@Override
 	public void setBatchEntryDAO(final BatchEntryDAO batchEntryDAO) {
 		this.batchEntryDAO = batchEntryDAO;
 	}
-	
+
 	@Override
 	public Batch createBatch(final Batch batch, final BatchOperationType batchOperationType) {
-		
+
 		this.batchDAO.save(batch);
 		final BatchEntry batchEntry = new BatchEntry(batch, batchOperationType, batch.getPackageQuantityUnits());
 		this.batchEntryDAO.save(batchEntry);
-		
+
 		return batch;
 	}
-	
+
 	@Override
 	public void createWasteDrug(final DrugOrder drugOrder, final Location location, final Double quantity,
-	        final Date date) throws Exception {
-		
+			final Date date) throws Exception {
+
 		final List<Batch> batches = this.batchDAO.findByDrugAndLocationAndNotExpiredDate(drugOrder.getDrug(), location,
-		    date, false);
-		
+				date, false);
+
 		if (batches.isEmpty()) {
-			
+
 			throw new APIException(Context.getMessageSourceService().getMessage("inventorypoc.error.noStockAvailablity",
-			    new String[] { drugOrder.getDrug().getDisplayName() }, Context.getLocale()));
+					new String[] { drugOrder.getDrug().getDisplayName() }, Context.getLocale()));
 		} else {
+
 			this.processForExistingBatches(drugOrder, batches, quantity);
 		}
 	}
-	
+
 	private void processForExistingBatches(final DrugOrder drugOrder, final List<Batch> batches,
-	        final Double quantity) {
+			final Double quantity) {
 		Double wastedQuantity = quantity;
-		
+
 		for (final Batch batch : batches) {
 			if (wastedQuantity.compareTo(BatchServiceImpl.ZERO_DOUBLE_VALUE) > 0) {
-				
+
 				Double entryQuantity;
 				if (wastedQuantity.compareTo(batch.getRemainPackageQuantityUnits()) > 0) {
 					entryQuantity = wastedQuantity - batch.getRemainPackageQuantityUnits();
@@ -98,76 +99,76 @@ public class BatchServiceImpl extends BaseOpenmrsService implements BatchService
 				this.batchEntryDAO.create(drugOrder, batch, BatchOperationType.DISTRIBUTION, entryQuantity);
 				this.batchDAO.decreaseRemainPackageQuantityUnits(batch, entryQuantity);
 				wastedQuantity = wastedQuantity - entryQuantity;
-				
+
 			} else {
 				throw new APIException(
-				        Context.getMessageSourceService().getMessage("inventorypoc.error.insuficientStock.availability",
-				            new String[] { drugOrder.getDrug().getDisplayName() }, Context.getLocale()));
+						Context.getMessageSourceService().getMessage("inventorypoc.error.insuficientStock.availability",
+								new String[] { drugOrder.getDrug().getDisplayName() }, Context.getLocale()));
 			}
 		}
 	}
-	
+
 	@Override
 	public void reverseWastedDrug(final DrugOrder drugOrder) throws Exception {
-		
+
 		final EncounterType encounterType = Context.getEncounterService()
-		        .getEncounterTypeByUuid(MappedEncounterTypes.DISPENSATION_ENCOUNTER_TYPE);
-		
+				.getEncounterTypeByUuid(MappedEncounterTypes.DISPENSATION_ENCOUNTER_TYPE);
+
 		final BatchEntry batchEntry = this.batchEntryDAO.findByDispensedOrder(drugOrder, encounterType, false);
 		this.batchEntryDAO.reverse(batchEntry);
 	}
-	
+
 	@Override
 	public List<Batch> findBatchesByDrugAndLocationAndNotExpiredDate(final Drug drug, final Location location,
-	        final Date date) {
+			final Date date) {
 		return this.batchDAO.findByDrugAndLocationAndNotExpiredDate(drug, location, date, false);
 	}
-	
+
 	@Override
 	public List<Batch> findAllAvailableStock(final Location location, final Date currentDate) {
-		
+
 		return this.batchDAO.findByLocationAndAvailableQuantity(location, currentDate, false);
 	}
-	
+
 	@Override
 	public Batch adjustBatchCurrentQuantity(final Batch batch, final Double newRemainPackageQuantityUnits) {
-		
+
 		this.batchValidator.validateAdjustments(batch, newRemainPackageQuantityUnits);
-		
+
 		Double deltaQuantity = newRemainPackageQuantityUnits - batch.getRemainPackageQuantityUnits();
 		batch.setRemainPackageQuantityUnits(batch.getRemainPackageQuantityUnits() + deltaQuantity);
-		
+
 		BatchOperationType batchOperationType = BatchOperationType.POSETIVE_ADJUSTMENT;
 		if (deltaQuantity < 0) {
 			batchOperationType = BatchOperationType.NEGATIVE_ADJUSTMENT;
 			deltaQuantity = deltaQuantity * -1;
 		}
-		
+
 		final BatchEntry batchEntry = new BatchEntry(batch, batchOperationType, deltaQuantity);
-		
+
 		this.batchEntryDAO.save(batchEntry);
 		this.batchDAO.save(batch);
-		
+
 		return batch;
 	}
-	
+
 	@Override
 	public Batch adjustBatchWithDeliverNoteItem(final Batch batch, final DeliverNoteItem deliverNoteItem) {
-		
+
 		final Double newQuantity = deliverNoteItem.getQuantity() * deliverNoteItem.getDrugPackage().getTotalQuantity();
-		
+
 		batch.setPackageQuantity(batch.getPackageQuantity() + deliverNoteItem.getQuantity());
 		batch.setPackageQuantityUnits(batch.getPackageQuantityUnits() + newQuantity);
 		batch.setRemainPackageQuantityUnits(batch.getRemainPackageQuantityUnits() + newQuantity);
-		
+
 		this.batchDAO.save(batch);
 		final BatchEntry batchEntry = new BatchEntry(batch, BatchOperationType.RECEIPT, newQuantity);
 		this.batchEntryDAO.save(batchEntry);
-		
+
 		return batch;
-		
+
 	}
-	
+
 	@Override
 	public Batch findBatchById(final Integer batchId) {
 		return this.batchDAO.findById(batchId);
